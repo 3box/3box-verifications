9cac09a538a4ab1aaa52123676751d41101058f8
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-01-10 10:56:22 -0300

    update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
-const path = require('path');
 // eslint-disable-next-line import/no-unresolved
+const path = require('path');
+const webpack = require('webpack')
 const slsw = require('serverless-webpack');
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
-      loaders: ['babel-loader'],
+      use: [{
+        loader: 'babel-loader'
+      }],
       include: __dirname,
       exclude: /node_modules/,
-    }],
+    }]
   },
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
-  },
+  }
 };
