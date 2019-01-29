9f81d0a62b3f96bf7312c95b59fe49ea5ba427e1
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-01-29 10:54:26 -0300

    verification flow using redis as a temp storage

--- src/lib/emailMgr.js
+++ src/lib/emailMgr.js
@@ -1,11 +1,47 @@
+const AWS = require('aws-sdk')
+
 class EmailMgr {
-    constructor() {
+  constructor (redisStore) {
+    this.ses = new AWS.SES()
+    this.redisStore = redisStore
+  }
+
+  async sendVerification (email) {
+    const code = this.generateCode()
+    await this.storeCode(email, code)
+    let params = {
+      Destination: {
+        ToAddresses: [email]
+      },
+      Source: 'verifications@3box.io' /* required */,
+      Template: 'TEMPLATE_NAME' /* required */,
+      TemplateData:
+        '{ "VERIFICATION_CODE":code }' /* required */,
+      ReplyToAddresses: ['verifications@3box.io']
     }
 
-    async sendVerification() {
-        throw new Error('not implemented yet')
-    }
+    let sendPromise = this.ses.sendTemplatedEmail(params).promise()
 
+    sendPromise
+      .then(function (data) {
+        console.log(data)
+      })
+      .catch(function (err) {
+        console.error(err, err.stack)
+      })
+  }
+
+  async checkVerification () {
+    throw new Error('not implemented yet')
+  }
+
+  generateCode () {
+    return Math.floor(100000 + Math.random() * 900000)
+  }
+
+  async storeCode (email, code) {
+    this.redisStore.write(email, code)
+  }
 }
 
-module.exports = EmailMgr;
+module.exports = EmailMgr
