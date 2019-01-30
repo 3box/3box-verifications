8fe8ee9329854a8ee5a3619be284676e728559d4
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-01-30 16:52:43 -0300

    mocked aws ses

--- src/lib/__tests__/emailMgr.test.js
+++ src/lib/__tests__/emailMgr.test.js
@@ -1,5 +1,6 @@
 const EmailMgr = require('../emailMgr')
 const NullStore = require('../store').NullStore
+const AWS = require('aws-sdk-mock')
 
 describe('EmailMgr', () => {
   let store
@@ -7,6 +8,14 @@ describe('EmailMgr', () => {
   let userEmail
   let userDid
   let userName
+
+  let mockResponse = {
+    ResponseMetadata: { RequestId: '6c4b41b4-24c1-11e9-a415-efc225a7e54a' },
+    MessageId: '01010168a0230a86-0e8ba89b-3339-4979-a138-d036e959192d-000000'
+  }
+  AWS.mock('SES', 'sendEmail', function (params, callback) {
+    callback(null, mockResponse)
+  })
 
   beforeAll(() => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
@@ -40,5 +49,9 @@ describe('EmailMgr', () => {
         expect(resp).toBeDefined()
         done()
       })
+  })
+
+  afterAll(() => {
+    AWS.restore('SES')
   })
 })
