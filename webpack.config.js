63ab7b86ea8f6b51d212cec27fce52fc6ec430f6
Cristóbal Castillo <cristobal.castillo@consensys.net>
2019-01-30 19:15:37 -0300

    fix webpack bug bundling redis

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 // eslint-disable-next-line import/no-unresolved
-const path = require('path');
+const path = require('path')
 const webpack = require('webpack')
-const slsw = require('serverless-webpack');
+const slsw = require('serverless-webpack')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+  externals: [nodeExternals()],
   module: {
     rules: [{
       test: /\.js$/,
@@ -13,12 +15,12 @@ module.exports = {
         loader: 'babel-loader'
       }],
       include: __dirname,
-      exclude: /node_modules/,
+      exclude: /node_modules/
     }]
   },
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
+    filename: '[name].js'
   }
-};
+}
